84b0e384f3d849ccee21f08c391f384737579a26
lesteven <le_steven@outlook.com>
2018-07-31 23:33:18 -0700

    fix string rotation bug

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.println("i " + i);
+      for (int j = 0; j < 10; j++) {
+        System.out.print(j + " " );
+        if (j == 5) break;
+      }
+      System.out.println();
+    }
+  }
+}
--- chapter1/StringRotation.java
+++ chapter1/StringRotation.java
@@ -1,27 +1,45 @@
-
+import java.util.ArrayList;
 
 public class StringRotation {
   private static boolean sameLength(String one, String two) {
     return one.length() == two.length();
   }
-  public static boolean isSubstring(String one, String two) {
+  private static boolean checkRotation(String one, String two) {
     char firstLetter = one.charAt(0);
-    int offset = two.indexOf(firstLetter);
-    if (sameLength(one, two)) { 
-      int len = one.length();
+    int len = one.length();
+    ArrayList<Integer> arr = findOffset(firstLetter, two);
+
+    int count = 0;
+    for (Integer offset : arr) {
       for (int i = 0; i < len; i++) {
         if (one.charAt(i) != two.charAt((i + offset) % len)) {
-          return false;
+          count++;
+          break;
         }
       }
-      return true;
+    }
+    return count != arr.size();
+  }
+  public static boolean isSubstring(String one, String two) {
+    if (sameLength(one, two)) { 
+      return checkRotation(one, two);
     }
     return false;
+  }
+  private static ArrayList<Integer> findOffset(char letter, String two) {
+    ArrayList<Integer> arr = new ArrayList<>();
+    for (int i = 0; i < two.length(); i++) {
+      if (letter == two.charAt(i)) arr.add(i); 
+    }
+    return arr;
   }
   public static void main(String[] args) {
     System.out.println(isSubstring("waterbottle","erbottlewat"));
     System.out.println(isSubstring("waterbottle","erbottlewaz"));
     System.out.println(isSubstring("waterbottle","erbottlew"));
+    System.out.println(isSubstring("title","tleti"));
+    System.out.println(isSubstring("aaajjj","aaajjj"));
+    System.out.println(isSubstring("aaajjj","jjaaaj"));
 
   }
 }
